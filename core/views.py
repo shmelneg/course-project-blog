ee7857430dc136bd7e839b0caabe5d90c82b6d4f
shmelneg <shmelneg@gmail.com>
2022-11-11 22:44:12 +0200

    initial commit

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.http.response import HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from blog.models import Post
+
+# Create your views here.
+def frontpage(request):
+    posts = Post.objects.all()
+    return render(request, 'core/frontpage.html', {'posts': posts})
+
+def about(request):
+    return render(request, 'core/about.html')
